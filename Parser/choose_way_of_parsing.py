5af0f680842df8b93c66c34cb8abae1293b37db5
Nikita <nekitos199@gmail.com>
2021-05-11 14:17:24 +0300

    simplifying

--- Parser/choose_way_of_parsing.py
+++ Parser/choose_way_of_parsing.py
@@ -1,24 +1,19 @@
-
 from base_class_parser import BaseClassParser
 
 
 def choose_way_of_parsing(url: str):
-    if 'https://zen.yandex.ru/' in url: 
-        return 'zen'
-    elif 'https://www.hi-fi.ru/' in url:
-        return 'hi-fi'
-    elif 'https://wroom.ru/' in url: 
-        return 'wroom'
-    elif 'https://www.autonews.ru/' in url: 
-        return 'autonews'
-    elif 'https://iz.ru/' in url: 
-        return 'izvestia'
-    elif 'https://www.championat.com/' in url:
-        return 'championat'
-    else: 
-        if BaseClassParser(url).text_from_page is not None: 
-          return  BaseClassParser(url).text_from_page 
-        else: 
-            raise 'NotImplementedYet'
-     
-         
+    for value in (
+        "https://zen.yandex.ru/",
+        "https://www.hi-fi.ru/",
+        "https://wroom.ru/",
+        "https://www.autonews.ru/",
+        "https://iz.ru/",
+        "https://www.championat.com/",
+    ):
+        if value in url:
+            return value
+        else:
+            if BaseClassParser(url).text_from_page is not None:
+                return BaseClassParser(url).text_from_page
+            else:
+                raise "NotImplementedYet"

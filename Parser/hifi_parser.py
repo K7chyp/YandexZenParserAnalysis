12f481d86333639d8a6daf758c0553fa735d95b2
Nikita <nekitos199@gmail.com>
2021-05-11 11:15:00 +0300

    Hi-Fi updated

--- Parser/hifi_parser.py
+++ Parser/hifi_parser.py
@@ -8,6 +8,7 @@ class HiFiPageParser(BaseClassPageParser):
     def __init__(self, url) -> None:
         super().__init__(url)
         self.get_content_from_page()
+        self.raiting = self.soup.find("span", {"class": "ratings__num"}).text
         self.browser.close()
 
     def get_content_from_page(self) -> None:
@@ -15,3 +16,9 @@ class HiFiPageParser(BaseClassPageParser):
             [text_part.text for text_part in self.soup.find_all("p")]
         )
         self.text_from_page: str = self.text_from_page
+        self.page_content: str = [
+            content.text.replace("\n", "").replace("\t", "")
+            for content in self.soup.find_all("div", {"class": "autor"})
+        ]
+        self.article_author = self.page_content[0]
+        self.publish_date = self.page_content[1]
--- Parser/main.py
+++ Parser/main.py
@@ -4,7 +4,7 @@ from hifi_parser import HiFiPageParser
 def main():
     print(HiFiPageParser(
         "https://www.hi-fi.ru/magazine/kino/8-nastolko-strashnykh-uzh/?utm_referrer=https%3A%2F%2Fzen.yandex.com"
-    ).text_from_page
+    ).raiting
 )
 
 if __name__ == "__main__":
